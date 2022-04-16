cd2c83ee399bd735a7264a4743dab66599bec39b
Benjamin Nyaoro <nyaorobenjamin@gmail.com>
2022-04-16 17:08:32 +0200

    All rspec items raking green.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,43 @@
 class BookmarksController < ApplicationController
+
+  before_action :set_list, only: [:new, :create]  
+  before_action :set_bookmark, only:[:destroy]
+
+  # routes to #new
+  def new
+		@bookmark = Bookmark.new
+	end
+  
+  # routes to #create
+  def create
+		@bookmark = Bookmark.new(bookmark_params)
+		@bookmark.list = @list
+    if @bookmark.save
+			redirect_to list_path(@list)
+		else
+			render :new, notice: "Bookmark was successfully created"
+		end
+	end
+
+  # routes to #destroy
+
+  def destroy		
+    @bookmark.destroy
+    redirect_to bookmark_path(@bookmark.movie)
+	end
+
+private
+
+  def bookmark_params
+		params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+	end
+
+  def set_bookmark
+		@bookmark = Bookmark.find(params[:id])
+	end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,38 @@
 class ListsController < ApplicationController
+
+  before_action :set_list, only: [:show]
+
+  # routes to #index
+  def index
+		@lists = List.all
+	end
+  # routes to #new
+  def new
+		@list = List.new
+	end
+  
+  # routes to #show
+  def show
+	end
+
+  # routes to #create
+  def create
+		@list = List.create(list_params)
+    if @list.save
+			redirect_to list_path(@list)
+		else
+			render :new, notice: "List was successfully created"
+		end
+	end
+
+  private
+  
+  def set_list
+		@list = List.find(params[:id])
+	end
+
+  def list_params
+		params.require(:list).permit(:name)
+	end
+
 end
